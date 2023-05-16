8c9df193ee73c8cd54eb156a1cd42785c7e4ae21
JoaquinTaborda <130505200+Avannz@users.noreply.github.com>
2023-05-16 20:58:43 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,23 +2,40 @@
 #include <stdlib.h>
 
 const int filas=3;
-const int columnas=2;
+const int columnas=3;
 
-void cargar(int matriz[filas][columnas]);
+void cargarMatriz(int matriz[filas][columnas]);
+void mostrarMatriz(int matriz[filas][columnas]);
+void cargarMatrizRandom(int matriz[filas][columnas], int random[filas][columnas]);
+float promedioMatriz(float matriz[filas][columnas]);
 
 int main()
 {
     int matriz[filas][columnas];
+    int matrizRandom[filas][columnas];
+    int matrizFloat[filas][columnas];
+    int suma;
+    float promedio;
 
 
-    cargar(matriz);
+    cargarMatriz(matriz);
+    printf("\n\nMatriz de orden 'mxn': \n");
+    mostrarMatriz(matriz);
 
+    cargarMatrizRandom(matriz, matrizRandom);
+    printf("\nMatriz(rand) de orden 'mxn': \n");
+    mostrarMatriz(matrizRandom);
+
+    suma = sumarMatriz(matriz);
+    printf("La suma de la matriz de orden 'mxn' es de: %d", suma);
+
+    promedio = promedioMatriz(matrizFloat);
+    printf("El promedio de la matriz de orden 'mxn' es de: %f", promedio);
 
 }
 
-void cargar(int matriz[filas][columnas])
+void cargarMatriz(int matriz[filas][columnas])
 {
-
     int i;
     int j;
     int num = 1;
@@ -34,8 +51,84 @@ void cargar(int matriz[filas][columnas])
             printf("Ingrese el dato que desea (columna): ");
             fflush(stdin);
             scanf("%i", &matriz[i][j]); // Importante los corchetes para esto. Es para poner el dato el la posicion que querramos.
-
         }
         num++;
     }
+}
+
+void mostrarMatriz(int matriz[filas][columnas])
+{
+
+    int i;
+    int j;
+    int num = 1;
+
+    for(i = 0; i < filas; i++)
+    {
+
+        for(j = 0; j < columnas; j++)
+        {
+            printf("| %d |", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void cargarMatrizRandom(int matriz[filas][columnas], int random[filas][columnas])
+{
+
+    int i;
+    int j;
+    int num = 1;
+
+    for(i = 0; i < filas; i++)
+    {
+        for(j = 0; j < columnas; j++)
+        {
+            random [i][j] = rand ()% 100+1;
+        }
+        num++;
+    }
+}
+
+int sumarMatriz(int matriz[filas][columnas]){
+
+    int i;
+    int j;
+    int suma = 0;
+    int num = 1;
+
+    for(i = 0;i < filas; i++){
+
+        for(j = 0;j < columnas; j++){
+
+            suma = suma + matriz[i][j];
+        }
+
+        num++;
+    }
+    return suma;
+}
+
+float promedioMatriz(float matriz[filas][columnas]){
+
+    int i;
+    int j;
+    float suma = 0;
+    float promedio = 0;
+
+    for(i = 0; i < filas; i++){
+
+        for(j = 0; j < columnas; j++){
+
+            suma = suma + matriz[i][j];
+
+        }
+
+    }
+
+    promedio = suma / (filas+columnas);
+
+    return promedio;
+
 }
