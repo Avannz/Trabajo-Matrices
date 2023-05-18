8d3ed6ecf4ca01d7f28d78177d197840081613d6
JoaquinTaborda <130505200+Avannz@users.noreply.github.com>
2023-05-18 12:34:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,57 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 const int filas=2;
+const int filasC=3;
 const int columnas=2;
+const int columnasC=30;
+const int dim=5;
 
 void cargarMatriz(int matriz[filas][columnas]);
 void mostrarMatriz(int matriz[filas][columnas]);
 void cargarMatrizRandom(int matriz[filas][columnas], int random[filas][columnas]);
 float promedioMatriz(int matriz[filas][columnas]);
 int buscarMatriz(int matriz[filas][columnas], int dato);
+int cargarArregloSt(char arreglo[filasC][columnasC], int dim);
+void mostrarArregloSt(char arreglo[filasC][columnasC], int val);
 
 int main()
 {
     int matriz[filas][columnas];
     int matrizRandom[filas][columnas];
     int matrizFloat[filas][columnas];
+    char arreglo[filasC][columnasC];
+    char palabra[columnasC];
     int suma;
     int flag;
     int dato;
+    int validos;
     float promedio;
 
-    cargarMatriz(matriz);
-    printf("\n\nMatriz de orden 'mxn': \n");
-    mostrarMatriz(matriz);
+//    cargarMatriz(matriz);
+//    printf("\n\nMatriz de orden 'mxn': \n");
+//    mostrarMatriz(matriz);
+//
+//    cargarMatrizRandom(matriz, matrizRandom);
+//    printf("\nMatriz(rand) de orden 'mxn': \n");
+//    mostrarMatriz(matrizRandom);
+//
+//    suma = sumarMatriz(matriz);
+//    printf("\nLa suma de la matriz de orden 'mxn' es de: %d", suma);
+//
+//    promedio = promedioMatriz(matriz);
+//    printf("\n\nEl promedio de la matriz de orden 'mxn' es de: %.2f", promedio);
+//
+//
+//    printf("\nIngrese un dato para buscar en la matriz:");
+//    fflush(stdin);
+//    scanf("%d", &dato);
+//    flag = buscarMatriz(matriz, dato);
+//
+//
+//
+//    if(flag == 1)
+//    {
+//
+//        printf("\nEl dato se encuentra dentro de la matriz");
+//
+//    }
+//    else
+//    {
+//
+//        printf("\nEl dato no se encuentra dentro de la matriz\n\n");
+//
+//    }
 
-    cargarMatrizRandom(matriz, matrizRandom);
-    printf("\nMatriz(rand) de orden 'mxn': \n");
-    mostrarMatriz(matrizRandom);
+    int val = cargarArregloSt(arreglo, dim);
+    mostrarArregloSt(arreglo,val);
 
-    suma = sumarMatriz(matriz);
-    printf("\nLa suma de la matriz de orden 'mxn' es de: %d", suma);
-
-    promedio = promedioMatriz(matriz);
-    printf("\n\nEl promedio de la matriz de orden 'mxn' es de: %.2f", promedio);
-
-
-    printf("\nIngrese un dato para buscar en la matriz: ");
+    //cargarArregloSt(palabra, dim);
+    printf("\ningrese palabra a buscar: ");
     fflush(stdin);
-    scanf("%d", &dato);
-    flag = buscarMatriz(matriz, dato);
+    gets(palabra);
 
-    if(flag == 1)
-    {
+    flag = encontrarPalabra(arreglo, val, palabra);
 
-        printf("\nEl dato se encuentra dentro de la matriz");
+        if(flag == -1){
 
-    }
-    else
-    {
+            printf("La palabra no fue encontrada");
 
-        printf("\nEl dato no se encuentra dentro de la matriz");
 
-    }
+        }else{
+
+            printf("La palabra se encuentra en la fila %d", flag);
+
+        }
+
 
 
 }
@@ -191,4 +224,69 @@ int buscarMatriz(int matriz[filas][columnas], int dato)
 
 }
 
+int cargarArregloSt(char arreglo[filasC][columnasC], int dim){
 
+    int i = 0;
+    char letra = 's';
+    char palabra[columnasC];
+
+
+    while(i < filasC && letra =='s'){
+
+        printf("\nIngresa una palabra: ");
+        fflush(stdin);
+        scanf("%s", &palabra);
+
+        strcpy(arreglo[i], palabra);
+
+        printf("\nIngresa 's' para continuar: ");
+        fflush(stdin);
+        scanf("%c", &letra);
+        i++;
+    }
+    return i;
+
+}
+
+
+
+void mostrarArregloSt(char arreglo[filasC][columnasC], int val){
+
+    int i = 0;
+
+    for(i = 0; i < val; i++){
+
+        printf("%s ", arreglo[i]);
+
+    }
+}
+
+int encontrarPalabra(char arreglo[filasC][columnasC], int validos, char palabra[]){
+
+    int i = 0;
+    int flag = 0;
+    int pos;
+
+    while(i < filasC && flag != 1){
+
+        if(strcmp(arreglo[i],palabra) == 0){
+
+
+             pos = i;
+             flag = 1;
+
+        }else{
+
+            flag = -1;
+
+        }
+
+        i++;
+
+    }
+    if(flag == 1){
+        flag = pos;
+    }
+
+    return flag;
+}
